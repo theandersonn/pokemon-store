70c1d33d92f45e8ba1b7615c9bc1c7e71a2ca73e
theandersonn <theandersonn@gmail.com>
2020-05-05 14:28:10 -0300

    Update line-height button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,7 +4,7 @@ import { shade } from 'polished';
 export const ButtonElement = styled.button`
   max-width: 270px;
   width: 100%;
-  line-height: 38px;
+  line-height: 24px;
   background-color: #000;
   color: #fff;
   border: none;
@@ -12,6 +12,10 @@ export const ButtonElement = styled.button`
   text-transform: uppercase;
   font-weight: 700;
   transition: background-color 0.2s;
+
+  @media (min-width: 432px) {
+    line-height: 38px;
+  }
 
   &:hover {
     background: ${shade(0.2, '#FFCB07')};
